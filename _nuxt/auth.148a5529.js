f713345074ab4ce017d8cc8141b9efd716920495
John Martini <jokermartini@gmail.com>
2023-06-07 13:43:46 -0400

    hunter

---
+++ _nuxt/auth.148a5529.js
@@ -0,0 +1,1 @@
+import{H as e,e as s,D as l,o as r}from"./entry.a6ec158e.js";const a={class:"flex flex-col justify-center min-h-full py-12"},u={__name:"auth",setup(o){return e({title:"Ghost Asset Browser",htmlAttrs:{},bodyAttrs:{class:"w-full h-full text-neutral-300 bg-gray-800"}}),(t,c)=>(r(),s("div",a,[l(t.$slots,"default")]))}};export{u as default};
